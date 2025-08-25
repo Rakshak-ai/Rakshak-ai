b57a0b95cfb54742dc9c886d674ec6ade28b263e
Rakshak-ai <rakshakaisos@gmail.com>
2025-08-25 14:33:17 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/rakshak/ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/rakshak/ai/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.rakshak.ai
+package com.example.rakshak;
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterActivity;
 
-class MainActivity: FlutterActivity() {
+public class MainActivity extends FlutterActivity {
 }
